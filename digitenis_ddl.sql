69b0f48669da5dbe64556fd688bce58090c8ce9d
matbragan <matheus.bs.oliveira@gmail.com>
2023-05-09 20:39:06 -0300

    feat: sql commands

---
+++ digitenis_ddl.sql
@@ -0,0 +1,63 @@
+create database digitenis;
+
+create table fornecedor (
+    id_fornecedor int,
+    cnpj varchar(14),
+    nome varchar(50),
+    email varchar(50),
+    primary key (id_fornecedor)
+);
+
+create table telefone (
+    id_fornecedor int,
+    telefone varchar(11)
+    primary key (id_fornecedor, telefone),
+    foreign key (id_fornecedor) references fornecedor(id_fornecedor)
+);
+
+create table funcionario (
+    id_funcionario int,
+    nome varchar(50),
+    cpf varchar(11),
+    cargo varchar(50),
+    primary key (id_funcionario)
+);
+
+create table produto (
+    id_produto int,
+    nome varchar(50),
+    tamanho varchar(2), -- int faz mais sentido?
+    categoria varchar(50),
+    valor_compra float,
+    valor_venda float,
+    id_fornecedor int,
+    primary key (id_produto),
+    foreign key (id_fornecedor) references fornecedor(id_fornecedor)
+);
+
+create table venda (
+    id_venda int,
+    valor_total float,
+    "data" date,
+    hora time,
+    id_funcionario int,
+    primary key (id_venda),
+    foreign key (id_funcionario) references funcionario(id_funcionario)
+);
+
+create table item_venda (
+    id_produto int,
+    id_venda int,
+    quantidade int,
+    desconto float,
+    primary key (id_produto, id_venda),
+    foreign key (id_produto) references produto(id_produto),
+    foreign key (id_venda) references venda(id_venda)
+);
+
+create table estoque (
+    id_produto int,
+    quantidade int,
+    primary key (id_produto),
+    foreign key (id_produto) references produto(id_produto)
+);
---
+++ digitenis_dml.sql
@@ -0,0 +1,26 @@
+select * from fornecedor;
+
+insert into fornecedor 
+(id_fornecedor, cnpj, nome, email)
+values
+(	1,	"12345678000112",	"Nike",	"contato@nike.com.br"	),
+(	2,	"98765432000198",	"Adidas",	"contato@adidas.com.br"	),
+(	3,	"11222333000101",	"Puma",	"contato@puma.com.br"	),
+(	4,	"99888777000155",	"Mizuno",	"contato@mizuno.com.br"	),
+(	5,	"88777666000123",	"Asics",	"contato@asics.com.br"	),
+(	6,	"66555444000109",	"Under Armour",	"contato@underarmour.com.br"	),
+(	7,	"44333222000187",	"New Balance",	"contato@newbalance.com.br"	),
+(	8,	"33444555000134",	"Skechers",	"contato@skechers.com.br"	),
+(	9,	"22555666000112",	"Reebok",	"contato@reebok.com.br"	),
+(	10,	"77888999000154",	"Fila",	"contato@fila.com.br"	),
+(	11,	"66777888000123",	"Brooks",	"contato@brooks.com.br"	),
+(	12,	"55666777000143",	"Salomon",	"contato@salomon.com.br"	),
+(	13,	"44777888000165",	"Hoka One One",	"contato@hokaoneone.com.br"	),
+(	14,	"33888999000176",	"On Running",	"contato@on-running.com.br"	),
+(	15,	"22999000000190",	"La Sportiva",	"contato@lasportiva.com.br"	),
+(	16,	"11000111000122",	"Merrell",	"contato@merrell.com.br"	),
+(	17,	"99111222000133",	"Newton Running",	"contato@newtonrunning.com.br"	),
+(	18,	"88222333000144",	"Altra",	"contato@altrarunning.com.br"	),
+(	19,	"77333444000165",	"Topper",	"contato@topper.com.br"	),
+(	20,	"66444555000176",	"K-Swiss",	"contato@kswiss.com.br"	)
+;
