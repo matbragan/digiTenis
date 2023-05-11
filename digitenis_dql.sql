3b378735bdf45e91ae3a40042abb0f6870fb979e
matbragan <matheus.bs.oliveira@gmail.com>
2023-05-10 23:08:24 -0300

    update digitenis db

---
+++ digitenis_dql.sql
@@ -0,0 +1,13 @@
+select * from fornecedor;
+
+select * from produto;
+
+select * from funcionario;
+
+select * from telefone;
+
+select * from estoque;
+
+select * from venda;
+
+select * from item_venda;
